6b6a859a7a887a2fc7089320ae574f8059931f57
Maaz Khan <maazk8448@gmail.com>
2024-08-18 23:25:49 +0530

    Navbaar Complete

--- Portfolio/asset/js/script.js
+++ Portfolio/asset/js/script.js
@@ -1,6 +1,6 @@
 let menu = document.querySelector(".menubar");
 function menubar_open() {
-    menu.style.display = "inline-block";
+    menu.style.display = "block";
 }
 function menubar_close() {
     menu.style.display = "none";
